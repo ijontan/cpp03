c0a85e768d46ff9ab51c146baf1e5462790b1907
ijontan <85871000+ijontan@users.noreply.github.com>
2023-11-29 12:46:25 +0800

    update ex01 test

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -7,5 +7,9 @@ int main(void)
     ScavTrap scavTrap("ScavTrap");
 
     scavTrap.attack("target");
+    scavTrap.takeDamage(10);
+    scavTrap.beRepaired(10);
+    scavTrap.guardGate();
+    scavTrap.guardGate();
     return 0;
 }
