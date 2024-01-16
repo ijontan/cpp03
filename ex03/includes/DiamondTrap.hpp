1839170a99aafac2bed58f2fee605273d9114ad4
ijontan <85871000+ijontan@users.noreply.github.com>
2024-01-16 22:02:51 +0800

    update diamondtrap

--- ex03/includes/DiamondTrap.hpp
+++ ex03/includes/DiamondTrap.hpp
@@ -15,9 +15,9 @@ public:
     ~DiamondTrap(void);
     DiamondTrap(const DiamondTrap &src);
     DiamondTrap &operator=(const DiamondTrap &rhs);
-    virtual void attack(const std::string &target);
-    virtual void takeDamage(unsigned int amount);
-    virtual void beRepaired(unsigned int amount);
+    void attack(const std::string &target) override;
+    void takeDamage(unsigned int amount) override;
+    void beRepaired(unsigned int amount) override;
     void whoAmI(void);
 };
 
